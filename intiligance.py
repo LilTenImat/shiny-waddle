c6fb6e9fb6c76b95d821282740567e6b9adf16a4
clickname <20002mc@gmail.com>
2021-02-03 23:44:57 +0300

    path_fix

--- intiligance.py
+++ intiligance.py
@@ -5,28 +5,27 @@ import speech_recognition as speech_recog
 from nltk.stem.snowball import RussianStemmer
 from pickle import load
 from sklearn.preprocessing import normalize
-from logger import get_logger
+from logger import get_commands_logger, get_ml_logger
 
 mic = speech_recog.Microphone()
 recog = speech_recog.Recognizer()
 stemmer = RussianStemmer(False)
-solver = load(open('finalized_model.sav', 'rb'))
-tokinizer = load(open('tokenizer.sav', 'rb'))
+solver = load(open('config/finalized_model.sav', 'rb'))
+tokinizer = load(open('config/tokenizer.sav', 'rb'))
 
-func_logger = get_logger('func_logger')
-func_logger.error('мне плохо',extra={'func': '23123213'})
-
+comm_logger = get_commands_logger('commands_logger')
+ml_logger = get_ml_logger('ml_logger')
 def open_browser():
     os.system('xdg-open http:// 2>>logs/functions_errors.log')
-    func_logger.info("Browser opened")
+    comm_logger.info("Browser opened")
 
 def open_explorer():
     os.system('xdg-open . 2>>logs/functions_errors.log')
-    func_logger.info("Explorer opened")
+    comm_logger.info("Explorer opened")
 
 def shutdown():
     os.system('shutdown -h now 2>>logs/functions_errors.log')
-    func_logger.info("Shutdowning")
+    comm_logger.info("Shutdowning")
 
 Labels2Commands = {0: open_explorer, 1: open_browser, 2: shutdown}
 
--- logger.py
+++ logger.py
@@ -3,14 +3,26 @@ import logging
 _error_format = f"%(asctime)s - [%(func)s] - %(name)s - %(message)s"
 _info_format = f"%(asctime)s - %(message)s"
 
-def get_function_errors_handler():
-    file_handler = logging.FileHandler("logs/functions_errors.log")
+def commands_errors_handler():
+    file_handler = logging.FileHandler("logs/commands_errors.log")
     file_handler.setLevel(logging.ERROR)
     file_handler.setFormatter(logging.Formatter(_error_format))
     return file_handler
 
-def get_commands_handler():
-    file_handler = logging.FileHandler("logs/commands.log")
+def commands_info_handler():
+    file_handler = logging.FileHandler("logs/commands_info.log")
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logging.Formatter(_info_format))
+    return file_handler
+
+def ml_info_handler():
+    file_handler = logging.FileHandler("logs/ml_info.log")
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logging.Formatter(_info_format))
+    return file_handler
+
+def ml_errors_handler():
+    file_handler = logging.FileHandler("logs/errors_info.log")
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(logging.Formatter(_info_format))
     return file_handler
@@ -21,10 +33,17 @@ def get_stream_handler():
     stream_handler.setFormatter(logging.Formatter(_error_format))
     return stream_handler
 
-def get_logger(name):
+def get_commands_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
-    logger.addHandler(get_function_errors_handler())
-    logger.addHandler(get_commands_handler())
+    logger.addHandler(commands_errors_handler())
+    logger.addHandler(commands_info_handler())
     # logger.addHandler(get_stream_handler())
+    return logger
+
+def get_ml_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(ml_errors_handler())
+    logger.addHandler(ml_info_handler())
     return logger
