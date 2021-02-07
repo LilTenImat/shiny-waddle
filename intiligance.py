dc12450f7d5ffea1d96b7a0b31b28064d1b9b20e
cl1ckname <20002mc@gmail.com>
2021-02-07 19:39:49 +0300

    weather and new system of returns

--- intiligance.py
+++ intiligance.py
@@ -1,11 +1,16 @@
 import os
 import pyaudio
 import wave
+import requests
+import json
 import speech_recognition as speech_recog
 from nltk.stem.snowball import RussianStemmer
 from pickle import load
 from sklearn.preprocessing import normalize
 from logger import get_commands_logger, get_ml_logger
+from bs4 import BeautifulSoup
+
+PATH = os.getcwd()
 
 mic = speech_recog.Microphone()
 recog = speech_recog.Recognizer()
@@ -17,21 +22,31 @@ ml_logger = get_ml_logger('ml_logger')
 def open_browser():
     os.system('xdg-open http:// 2>>logs/functions_errors.log')
     comm_logger.info("Browser opened")
+    return (2, 'Открываю браузер')
 
 def open_explorer():
     os.system('xdg-open . 2>>logs/functions_errors.log')
     comm_logger.info("Explorer opened")
+    return (0,'Проводник')
 
 def shutdown():
     os.system('shutdown -h now 2>>logs/functions_errors.log')
     comm_logger.info("Shutdowning")
+    return (1,'Выключение')
 
 def symbol():
-    print('mnogo')
+    return (3,'mnogo')
+
+def weather():
+    ip = requests.get('https://ip.beget.ru').text[:-1]
+    send_url = f'http://ipinfo.io/{ip}/geo?token=3edfdea826f009'
+    r = requests.get(send_url).json()
+    city = r['city'].replace(' ','%20')
+    api_key = json.load(open(PATH+'/config/settings.json'))['owm_api_key']
+    print(requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city},ru&appid={api_key}').text)
 
 
-
-Labels2Commands = {0: open_explorer, 1: shutdown, 2: open_browser, 3: symbol}
+Labels2Commands = {0: open_explorer, 1: shutdown, 2: open_browser, 3: symbol, 4: weather}
 
 class Worker():
     def __init__(self):
@@ -46,13 +61,12 @@ class Worker():
                 probabilities = solver.predict_proba([stemmed_text])
                 print(probabilities)
                 if probabilities[0][command] > 0.5:
-                    Labels2Commands[command]()
-                    return text
+                    return Labels2Commands[command]()
                 else:
-                    return text
+                    return (-1, text)
             except Exception as e:
                 print("Error: " + str(e))
-                return 'err'
+                return (-2, e)
         with mic as audio_file:
             print("Speak Please")
 
@@ -70,10 +84,9 @@ class Worker():
                 probabilities = solver.predict_proba([stemmed_text])
                 print(probabilities)
                 if probabilities[0][command] > 0.5:
-                    Labels2Commands[command]()
-                    return text
+                    return Labels2Commands[command]()
                 else:
-                    return text
+                    return (-1, text)
             except Exception as e:
                 print("Error: " + str(e))
-                return 'err'
+                return (-2, e)
