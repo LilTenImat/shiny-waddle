54886353edcefd59e53a864f45f156c8fdf2ab7b
cl1ckname <20002mc@gmail.com>
2021-02-07 18:11:58 +0300

    Revert "correct path and text recog"
    This reverts commit bf1eeb605bb1a62ba6064c693340fcb13aed4068.

--- intiligance.py
+++ intiligance.py
@@ -1,32 +1,29 @@
 import os
 import pyaudio
 import wave
-import json
 import speech_recognition as speech_recog
 from nltk.stem.snowball import RussianStemmer
 from pickle import load
+from sklearn.preprocessing import normalize
 from logger import get_commands_logger, get_ml_logger
-
-PATH = os.getcwd()
-settings = json.load(PATH+'config/settings.json')
 
 mic = speech_recog.Microphone()
 recog = speech_recog.Recognizer()
 stemmer = RussianStemmer(False)
-solver = load(open(PATH+r'config/RFC.sav','rb'))
+solver = load(open(r'config/RFC.sav','rb'))
 
 comm_logger = get_commands_logger('commands_logger')
 ml_logger = get_ml_logger('ml_logger')
 def open_browser():
-    os.system('xdg-open http:// 2>>'+PATH+'logs/functions_errors.log')
+    os.system('xdg-open http:// 2>>logs/functions_errors.log')
     comm_logger.info("Browser opened")
 
 def open_explorer():
-    os.system('xdg-open . 2>>'+PATH+'logs/functions_errors.log')
+    os.system('xdg-open . 2>>logs/functions_errors.log')
     comm_logger.info("Explorer opened")
 
 def shutdown():
-    os.system('shutdown -h now 2>>'+PATH+'logs/functions_errors.log')
+    os.system('shutdown -h now 2>>logs/functions_errors.log')
     comm_logger.info("Shutdowning")
 
 def symbol():
@@ -39,23 +36,7 @@ Labels2Commands = {0: open_explorer, 1: shutdown, 2: open_browser, 3: symbol}
 class Worker():
     def __init__(self):
         pass
-    def on_command(self, text=None):
-        if text:
-            try:
-                print('Stemming...')
-                stemmed_text = stemmer.stem(text)
-                print(stemmed_text)
-                command = solver.predict([stemmed_text])[0]
-                probabilities = solver.predict_proba([stemmed_text])
-                print(probabilities)
-                if probabilities[0][command] > 0.5:
-                    Labels2Commands[command]()
-                    return text
-                else:
-                    return text
-            except Exception as e:
-                print("Error: " + str(e))
-                return 'err'
+    def on_command(self):
         with mic as audio_file:
             print("Speak Please")
 
@@ -76,7 +57,7 @@ class Worker():
                     Labels2Commands[command]()
                     return text
                 else:
-                    return text
+                    return ''
             except Exception as e:
                 print("Error: " + str(e))
                 return 'err'
