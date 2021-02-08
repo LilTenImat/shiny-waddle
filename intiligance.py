c0765bc5688d4cae61814535a8dec556c33d7756
cl1ckname <20002mc@gmail.com>
2021-02-08 20:38:30 +0300

    add terminal command

--- intiligance.py
+++ intiligance.py
@@ -43,15 +43,20 @@ def weather():
     r = requests.get(send_url).json()
     city = r['city'].replace(' ','%20')
     api_key = json.load(open(PATH+'/config/settings.json'))['owm_api_key']
-    print(requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city},ru&appid={api_key}').text)
+    return (4,requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city},ru&appid={api_key}').text)
 
+def open_terminal():
+    os.system('gnome-terminal')
+    return (5,'term opened')
 
-Labels2Commands = {0: open_explorer, 1: shutdown, 2: open_browser, 3: symbol, 4: weather}
+Labels2Commands = {0: open_explorer, 1: shutdown, 2: open_browser, 3: symbol, 4: weather, 5: open_terminal}
 
 class Worker():
     def __init__(self):
         pass
-    def on_command(self, text=None):
+    
+    @staticmethod
+    def on_command(text=None):
         if text:
             try:
                 print('Stemming...')
