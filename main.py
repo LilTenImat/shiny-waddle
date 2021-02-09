30fe1d110e3e8b68b611fd5431ca338b56bcad36
LilTenImat <zorro7633@mail.ru>
2021-02-09 15:22:14 +0300

    minor

--- main.py
+++ main.py
@@ -15,12 +15,15 @@ worker = Worker()
 
 class CommandHandler(QtCore.QObject):
     running = False
-    finished = QtCore.pyqtSignal(str, str)
+    finished = QtCore.pyqtSignal(int, str)
 
     def run(self, text=None):
 
-        res = worker.on_command(text)
-        self.finished.emit(res, "user")
+        f, res = worker.on_command(text)
+        if f != -2:
+            self.finished.emit(f, res)
+        else:
+            self.finished.emit(f, 'Error')
 
 
 class Application(QMainWindow, design.Ui_MainWindow):
@@ -47,31 +50,23 @@ class Application(QMainWindow, design.Ui_MainWindow):
         self.lineEdit.returnPressed.connect(self.lineEdit.clear)
 
         self.tabWidget.setCurrentIndex(0)
-        if os.path.exists('settings.json'):
+
+        if os.path.exists('./config/settings.json'):
             self.dropSettings()
         else:
             self.saveSettings()
 
-    @QtCore.pyqtSlot(str, str)
-    def updateText(self, string, f):
-        if f == "user":
-            html = f'''
-                    <div style="text-align:center;">
-                        <div style="width:50%; background-color:#E0E0E2; border:000; border-radius:5px;">
-                            <p>{string}</p>
-                        </div>
-                    </div>
-                    '''
-        elif f == "ai":
-            html = f'''
-                    <div style="text-align:center;">
-                        <div style="width:50%; background-color:#494993; border:000; border-radius:5px;">
-                            <p>{string}</p>
-                        </div>
-                    </div>
-                    '''
+    @QtCore.pyqtSlot(int, str)
+    def updateText(self, f, string):
+        style = 
+        html = '<div style="%s"><p>%s</p></div>'
+        if f == -2:
+            self.textBrowser.append(html % (style, "Some error :("))
+        elif f == 4:
+            self.textBrowser.append(html % "Weather")
+        else:
+            self.textBrowser.append(html % string)
 
-        self.textBrowser.append(html)
 
     def saveSettings(self):
         settings = {}
@@ -79,11 +74,11 @@ class Application(QMainWindow, design.Ui_MainWindow):
         settings['cb2'] = self.checkBox_2.isChecked()
         settings['cb3'] = self.checkBox_3.isChecked()
         settings['cb4'] = self.checkBox_4.isChecked()
-        with open("settings.json", 'w') as setts:
+        with open("./config/settings.json", 'w') as setts:
             json.dump(settings, setts)
 
     def dropSettings(self):
-        with open("settings.json") as setts:
+        with open("./config/settings.json") as setts:
             settings = json.load(setts)
             self.checkBox.setChecked(settings['cb1'])
             self.checkBox_2.setChecked(settings['cb2'])
@@ -129,23 +124,12 @@ class Application(QMainWindow, design.Ui_MainWindow):
             self.hide()
             if self.checkBox_2.isChecked():
                 self.tray_icon.showMessage(
-                    "Shiny waddle",
-                    "Application was minimized to Tray! \nDouble click to activate",
-                    QSystemTrayIcon.Information,
-                    2000
+                    "Shiny waddle", "Application was minimized to Tray!",
+                    QSystemTrayIcon.Information, 2000
                 )
-        # elif self.checkBox_2.isChecked():
-        #     reply = QMessageBox.question(self, 'Quit',
-        #         "Are you sure to quit?", QMessageBox.Yes |
-        #         QMessageBox.No, QMessageBox.No)
-
-        #     if reply == QMessageBox.Yes:
-        #         event.accept()
-        #     else:
-        #         event.ignore()
         else:
-            self.thread.exit()
-            event.accept()
+            self.quit()
+            
 
     def quit(self):
         self.thread.exit()
